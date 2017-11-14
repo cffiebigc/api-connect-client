bbda5c70cb7d360c7d8a335a9e66f935d4bc2220
mariowise <mariolopezlandes@gmail.com>
2017-11-14 12:50:35 -0300

    feat(): Product#show + refactor #all

--- lib/api_connect_client/product.rb
+++ lib/api_connect_client/product.rb
@@ -3,11 +3,12 @@ class Product < Base
     @headers = { 'X-IBM-APIManagement-Context': 'bci-api-developers-sandbox-desarrollo.local-registry' }
   end
 
-  def list
+  def all
     get("/products")
   end
 
   def show(product_id)
+  	@headers['Accept'] = 'application/vnd.ibm-apim.product+json'
     get("/products/#{product_id}")
   end
 end
--- spec/api_connect_client/product_spec.rb
+++ spec/api_connect_client/product_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
-RSpec.describe 'product' do
-  describe '#list', vcr: { cassette_name: 'products' } do
-    it "returns a list of products" do
-      product = Product.new
-      expect(product.list).to be_an(Array)
+RSpec.describe Product do
+	let(:product) { described_class.new }
+
+  describe '#all', vcr: { cassette_name: 'products' } do
+    it "returns a list all of products" do
+      expect(product.all).to be_an(Array)
     end
   end
 
-  describe '#show', vcr: { cassette_name: 'products' } do 
-  	
+  describe '#show' do 
+  	it "return a specific product", vcr: { cassette_name: 'product-show' } do 
+  		result = product.show("5a0211480cf2b9aa64c1fd5d")
+  		expect(result["info"]["name"]).to eq("beneficios")
+  	end	
   end
 end
