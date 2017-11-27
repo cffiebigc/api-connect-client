a189a5182a7981f8628b18d6afd10cccc132a9ca
Carlos Fiebig <cffiebigc@gmail.com>
2017-11-27 16:03:18 -0300

    feat(user): Add admin credentials to user#create

--- lib/api_connect_client/user.rb
+++ lib/api_connect_client/user.rb
@@ -1,11 +1,13 @@
 class User < Base
-  def initialize
+  def initialize(admin_user, admin_pass)
     @headers = {
       'X-IBM-APIManagement-Context': ENV['BLUEMIX_CONTEXT']
     }
+    @admin_user = admin_user
+    @admin_pass = admin_pass
   end
 
   def create(body)
-    post('/users/register', body)
+    post('/users/register', body, @admin_user, @admin_pass)
   end
 end
