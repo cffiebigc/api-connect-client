8e64ffd9b1d0ed61861de58beaf2b1fe369383c5
Carlos Fiebig <cffiebigc@gmail.com>
2017-12-06 15:21:31 -0300

    task(lib) Move version to an independent file

--- lib/api_connect_client.rb
+++ lib/api_connect_client.rb
@@ -1,12 +1,11 @@
-require 'dotenv/load'
-require_relative './api_connect_client/base'
-require_relative './api_connect_client/product'
-require_relative './api_connect_client/user'
-require_relative './api_connect_client/application'
-require_relative './api_connect_client/manager'
-require_relative './api_connect_client/developer'
-require_relative './api_connect_client/config'
+require "api_connect_client/version"
 
-module ApiConnectClient
-  VERSION = '0.1.0'
+module ActiveImporter
+  require_relative './api_connect_client/base'
+  require_relative './api_connect_client/product'
+  require_relative './api_connect_client/user'
+  require_relative './api_connect_client/application'
+  require_relative './api_connect_client/manager'
+  require_relative './api_connect_client/developer'
+  require_relative './api_connect_client/config'
 end
---
+++ lib/api_connect_client/version.rb
@@ -0,0 +1,3 @@
+module ApiConnectClient
+  VERSION = '0.1.0'
+end
